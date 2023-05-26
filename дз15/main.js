5712ec718b4576b1ece03d18de0cedd853450680
emin9 <129058356+emin9@users.noreply.github.com>
2023-05-26 15:29:42 +0300

    Add files via upload

---
+++ дз15/main.js
@@ -0,0 +1,31 @@
+const a = 1
+function number(){
+    if(a < 10){
+        console.log("константа меньше 10")
+    }
+    else(
+        console.log("константа больше 10")
+    )
+
+}
+console.log(number())
+
+let x = 10
+let y = 7
+
+function math(){
+    if( x > y ){
+        console.log("x больше у")
+
+    }
+    else{
+    console.log("x не больше y");
+    }
+
+    
+}
+console.log(math());
+
+
+
+
